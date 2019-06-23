f86015f668997f2440613562c689417eecddac55
sylvo <49357334+sjonon@users.noreply.github.com>
2019-06-23 18:06:26 -0500

    added get request for omdb api and function to display movie results

--- liri.js
+++ liri.js
@@ -1,33 +1,41 @@
 console.log(process.argv);
 require("dotenv").config();
 var Spotify = require("node-spotify-api");
+var axios = require("axios");
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
-var bandUrl = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp"
-
 var action = process.argv[2];
 var query = process.argv[3];
+
+var spotify = new Spotify(keys.spotify);
+var bandUrl = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
+var movieUrl = "http://www.omdbapi.com/?t=" +query+ "&y=&plot=short&apikey=trilogy";
+
 console.log(query);
 
 switch (action){
 case "spotify-this-song":
     song();
+    break;
 case "concert-this":
     band();
+    break;
+case "movie-this":
+    movie();
+    break;
 }
 //functions to run program:
 //concert-this <band name here>: will search Bands in Town API and render the following info - name of venue, venue location, date of event (formatted to MM/DD/YYYY with moment)
     //query URL for bands in town: ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
 
     function band(){
-        axios.
-
-        console.log("Venue: ");
-        console.log("Location: ");
-        console.log("Date: ");
-
+        axios.get(bandUrl).then(function(response){
+            console.log(response);
+            console.log("Venue: ");
+            console.log("Location: ");
+            console.log("Date: ");
+        })
     }
 //spotify-this-song '<song name here>': Show info on Artist, song name, preview link of song from spotify, album the song is from, if no song provided then program
     //will return "The Sign" by Ace of Base. Spotify package will return these results
@@ -47,6 +55,25 @@ function song(){
 }
 //movie-this <movie name>: will return title of movie, year movie came out, imdb rating, rotten tomatoes rating, country where the movie was produced, 
     // language of the movie, plot, actors.  If no movie is entered program will return "Mr. Nobody" data. axios package will return these results
+console.log(movieUrl);
+function movie(){
+    axios.get(movieUrl).then(
+        function(response){
+        // console.log(response.data);
+        console.log("Title: "+response.data.Title);
+        console.log("Year of release: " +response.data.Year);
+        console.log("IMDB Rating: "+response.data.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: "+response.data.Ratings[1].Value);
+        console.log("Country: "+ (response.data.Country));
+        console.log("Movie language: "+ (response.data.Language));
+        console.log("Plot: "+response.data.Plot);
+        console.log("Actors: "+response.data.Actors);
+
+
+
+    })
+}
+
 //do-what-it-says: using the fs node package liri will take the text inside of random.txt and then use it to call one of the LIRI commands.
     //should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
     //Edit the .txt file to test out the feature for movie-this and concert-this
